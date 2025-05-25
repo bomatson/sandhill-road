263ff5ad7dcd9760e0cb7571e5912d61affbf57f
Robert Matson <bobby@payitoff.io>
2025-05-24 22:07:56 -0400

    fix: Replace ts-node dependency with compiled JavaScript entry point

--- index.js
+++ index.js
@@ -1,10 +1,6 @@
 #!/usr/bin/env node
 
-// Simple entry point for Sandhill Road game
-// This allows us to run the game with just "node index.js"
+// Entry point for Sandhill Road game
+// This runs the compiled JavaScript version
 
-// We use the ts-node runtime to execute the TypeScript directly
-require('ts-node/register');
-require('./src/cli/index.ts');
-
-console.log('Starting Sandhill Road...'); 
+require('./dist/cli/index.js'); 
